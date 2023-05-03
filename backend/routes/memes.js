093e05a5cf2fb3bae4c14a256563d324bc7e4619
ltadjer <94218573+ltadjer@users.noreply.github.com>
2023-05-03 14:37:35 +0200

    all memes test

--- backend/routes/memes.js
+++ backend/routes/memes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
-
+fs.readdir('./memes', (err, files) => {
+    if (err) throw err;
+    console.log(files);
+})
 	router.route('/')
 	.get(function(req, res) {
-		res.send('All Memes');
+		res.send(
+            
+        )
         console.log('All memes')
 	})
 	.post(function(req, res) {
