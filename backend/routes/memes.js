ab956c77415b79a89a3f4c909642a88b7e4c6b4d
ltadjer <94218573+ltadjer@users.noreply.github.com>
2023-05-05 09:17:28 +0200

    bug memes

--- backend/routes/memes.js
+++ backend/routes/memes.js
@@ -1,77 +1,90 @@
-const express = require('express');
-const fs = require('fs');
-const jimp = require('jimp');
+const express = require("express");
+const fs = require("fs");
+const jimp = require("jimp");
 const router = express.Router();
-const multer  = require('multer')
+const multer = require("multer");
+
+// Endpoint all meme
+router.route("/").get(function (res) {
+    fs.readdir("./memes", (err, memes) => {
+      if (err) throw err;
+      res.json(memes);
+    });
+  });
 
 // Stockage image
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './memes')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now()
-        const name = file.originalname.split('.')
-        const extension = file.originalname.split('.')
-        cb(null, name[0] + '-' + uniqueSuffix + '.' + extension[1])
-    }
-})
-const upload = multer({ storage: storage  })
+  destination: function (req, file, cb) {
+    cb(null, "./memes");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    const name = file.originalname.split(".");
+    const extension = file.originalname.split(".");
+    cb(null, name[0] + "-" + uniqueSuffix + "." + extension[1]);
+  },
+});
+const upload = multer({ storage: storage });
 
-    // Endpoint all meme
-	router.route('/')
-	.get(function(res) {
-            fs.readdir('./memes', (err, files) => {
-                if (err) throw err;
-                res.json(files);
-            } 
-        )
-	})
-    
-    // Endpoint create meme
-    router.route('/create')
-    .post(upload.single('meme'), (req, res, next) => {
-        const file = req.file
-        const textHaut = req.body.topText
-        const textBas = req.body.bottomText
-        if (!file) {
-          const error = new Error('Veuillez importer votre image')
-          error.httpStatusCode = 400
-          return next(error)
-        } else {
-            jimp.read(file.path) 
-                .then((meme) => {
-                    // TODO: Resizer l'image
-                    meme.resize(1000, jimp.AUTO)
-                    jimp.loadFont(jimp.FONT_SANS_32_WHITE).then(font => {
-                        meme.print(font, 0, 10, {
-                        text: textHaut,
-                        alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-                        alignmentY: jimp.VERTICAL_ALIGN_TOP
-                      }, meme.bitmap.width, meme.bitmap.height);
-                      meme.print(font, 0, -10, {
-                        text: textBas,
-                        alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-                        alignmentY: jimp.VERTICAL_ALIGN_BOTTOM
-                      }, meme.bitmap.width, meme.bitmap.height);
 
-                        meme.write(file.path);
-                      });
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-            res.send(file)
-        }
-    }) 
 
-    // Endpoint delete meme
-    router.route('/delete/:name')
-	.delete(function(req, res) {
-        fs.unlink('./memes/' + req.params.name, (err) => {
-            if (err) throw err;
-		    res.send(req.params.name+' a été supprimé.');
+// Endpoint create meme
+router.route("/create").post(upload.single("meme"), (req, res, next) => {
+  const file = req.file;
+  const textHaut = req.body.topText;
+  const textBas = req.body.bottomText;
+  if (!file) {
+    const error = new Error("Veuillez importer votre image");
+    error.httpStatusCode = 400;
+    return next(error);
+  } else {
+    jimp
+      .read(file.path)
+      .then((meme) => {
+        // TODO: Resizer l'image
+        meme.resize(1000, jimp.AUTO);
+        jimp.loadFont(jimp.FONT_SANS_32_WHITE).then((font) => {
+          meme.print(
+            font,
+            0,
+            10,
+            {
+              text: textHaut,
+              alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+              alignmentY: jimp.VERTICAL_ALIGN_TOP,
+            },
+            meme.bitmap.width,
+            meme.bitmap.height
+          );
+          meme.print(
+            font,
+            0,
+            -10,
+            {
+              text: textBas,
+              alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+              alignmentY: jimp.VERTICAL_ALIGN_BOTTOM,
+            },
+            meme.bitmap.width,
+            meme.bitmap.height
+          );
+
+          meme.write(file.path);
         });
-	});
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    res.send(file);
+  }
+});
+
+// Endpoint delete meme
+router.route("/delete/:name").delete(function (req, res) {
+  fs.unlink("./memes/" + req.params.name, (err) => {
+    if (err) throw err;
+    res.send(req.params.name + " a été supprimé.");
+  });
+});
 
 module.exports = router;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script>
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      memes: [],
+    };
+  },
+  mounted() {
+    this.getMemes();
+  },
+  methods: {
+    getMemes() {
+      axios
+        .get("http://127.0.0.1:3000/memes")
+        .then((response) => {
+          console.log(response.data);
+          this.memes = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <template>
   <div class="home">
     <h1>Accueil</h1>
+    <div>
+      <h2>Liste des memes</h2>
+      <ul>
+        <div v-for="meme in memes" :key="meme.name">
+          <img :src="meme.name" :alt="meme.name" />
+        </div>
+      </ul>
+    </div>
   </div>
 </template>
