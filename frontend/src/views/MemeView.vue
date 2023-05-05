0c7165d79c914b8bf66e58df33a3a8d84b759e67
Laura-Klery <lklery@yahoo.fr>
2023-05-05 16:09:09 +0200

    redirect to home

--- frontend/src/views/MemeView.vue
+++ frontend/src/views/MemeView.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from "axios";
 
+
 export default {
   data() {
     return {
@@ -23,6 +24,7 @@ export default {
       })
         .then(response => {
           console.log(response)
+          this.$router.push('/')
         })
         .catch(error => {
           console.log(error)
