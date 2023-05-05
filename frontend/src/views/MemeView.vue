bb8b28449e50cbb8b74fd76f8da91d5f67148dcf
ltadjer <94218573+ltadjer@users.noreply.github.com>
2023-05-05 13:46:53 +0200

    start create

--- frontend/src/views/MemeView.vue
+++ frontend/src/views/MemeView.vue
@@ -1,9 +1,38 @@
 <script>
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      meme: '',
+    };
+  },
+ 
+  methods: {
+  async handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    try {
+      const response = await axios.post('http://localhost:3000/memes/create', data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+};
 </script>
 
 <template>
   <div class="meme">
     <h1>Créer un meme</h1>
+    <form action="http://localhost:3000/memes/create" method="post">
+      <input type="file" name="meme">
+      <input type="text" name="topText">
+      <input type="text" name="bottomText">
+      <input type="submit" value="Envoyer">
+    </form>
   </div>
 </template>
