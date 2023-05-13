ff7142ea107c63d1f133c5035023650e567e0c8e
Laura-Klery <lklery@yahoo.fr>
2023-05-13 19:56:07 +0200

    feat(mainHeader): style - style in desktop done - start menu in mobile

--- frontend/src/components/MainHeader.vue
+++ frontend/src/components/MainHeader.vue
@@ -1,20 +1,19 @@
 <script>
-
 export default {
-
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
 }
 </script>
-
 <template>
-  <header class="main-header">
-    <div class="logo">
-      <RouterLink to="/"><img src="@/assets/img/logo.svg" alt="Logo Meme4Dev"></RouterLink>
-
-    </div>
-    <nav>
-        <RouterLink to="/">Accueil</RouterLink>
-        <RouterLink to="/new">Création</RouterLink>
-        <RouterLink to="/login"><MyButton>Connexion</MyButton></RouterLink>
+  <header class="mx-2 flex items-center justify-between">
+    <RouterLink to="/"><img src="@/assets/img/logo.svg" alt="Logo Meme4Dev" class="p-2"></RouterLink>
+    <nav class="lg:flex lg:justify-end lg:items-center space-x-24 lg:text-center">
+      <RouterLink to="/">Accueil</RouterLink>
+      <RouterLink to="/new">Création</RouterLink>
+      <RouterLink to="/login"><MyButton class="bg-buttons">Connexion</MyButton></RouterLink>
     </nav>
   </header>
 </template>
