26ea6382d1621e2dba0da1578723d4e89c90bdce
Laura-Klery <lklery@yahoo.fr>
2023-05-13 17:58:42 +0200

    feat(mainFooter): style and semantic

--- frontend/src/components/MainFooter.vue
+++ frontend/src/components/MainFooter.vue
@@ -1,5 +1,5 @@
 <template>
- <footer class="main-footer">
-  <p>Meme4Dev - Tout droit réservés - 2023 - <RouterLink to="/mentions-legales">Mentions légales</RouterLink></p>
+ <footer class="flex justify-around items-center text-center mx-8 pb-2 lg:mx-auto">
+  <p class="text-base">Meme4Dev - Tout droit réservés - 2023 - <RouterLink to="/mentions-legales">Mentions légales</RouterLink></p>
  </footer>
 </template>
